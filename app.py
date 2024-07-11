ae519040cb868cd3016a8f7efffd20715e19ad28
Artem Kovalevskyi <artem4852@MacBook-Pro--Artem.local>
2024-07-11 16:25:47 +0200

    prepare for nest

--- app.py
+++ app.py
@@ -135,5 +135,5 @@ def set_cookies():
     return response
 
 if __name__ == '__main__':
-    app.run(port=3000)
+    app.run(host="0.0.0.0", port=5000)
     # with open("2.json", "w") as f: json.dump(slack_search('"Instagram Scraper" "improvements and enhancements"'), f)
